3312407044ae8374b46bfb1ff7885ddb44cd6ffb
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-25 09:18:02 +0100

    Cosmetic changes

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,8 +1,7 @@
-feature 'add addres and title' do
-
-  scenario 'add site address and title to manager' do
+feature 'New links' do
+  scenario 'adding a new link' do
     visit '/links/new'
-    fill_in 'address', with: 'http://makersacademy.co.uk'
+    fill_in 'address', with: 'http://makersacademy.com'
     fill_in 'title', with: 'Makers Academy'
     click_button 'Submit'
     expect(page).to have_content('Makers Academy')
--- spec/features/list_spec.rb
+++ spec/features/list_spec.rb
@@ -1,11 +1,11 @@
-feature 'displays a list of links' do
-
-  scenario 'list of links displayed on homepage' do
+feature 'Viewing links' do
+  scenario 'existing links displayed on homepage' do
     Link.destroy
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    Link.create(url: 'http://www.google.com', title: 'Google')
+    Link.create(url: 'https://github.com/', title: 'GitHub')
     visit '/links'
     expect(page.status_code).to eq 200
-    puts page.text
     within 'ul#links' do
       expect(page).to have_content('Makers Academy')
     end
