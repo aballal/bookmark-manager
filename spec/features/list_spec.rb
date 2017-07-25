007eed304466d70718d4c1b4a9d2b37d47b6f391
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-25 13:54:50 +0100

    Added DatabaseCleaner to spec_helper

--- spec/features/list_spec.rb
+++ spec/features/list_spec.rb
@@ -1,6 +1,5 @@
 feature 'Viewing links' do
   scenario 'existing links displayed on homepage' do
-    Link.destroy
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     visit '/links'
     expect(page.status_code).to eq 200
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,39 @@
-require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
-
 require 'capybara/rspec'
+require 'database_cleaner'
 require 'simplecov'
 require 'simplecov-console'
+
+require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
 require './app/models/link'
 
 Capybara.app = BookmarkManager
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+    SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    puts
+    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  end
+end
