8a5d3f32414b846fab4adf928e6fc06529d2d036
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-26 17:12:06 +0100

    Added functionality to handle multiple tags

--- app/app.rb
+++ app/app.rb
@@ -22,8 +22,9 @@ class BookmarkManager < Sinatra::Base
 
   post '/links' do
     link = Link.new(title: params[:title], url: params[:url])
-    tag = Tag.first_or_create(name: params[:tags])
-    link.tags << tag
+    params[:tags].split.each do |tag_name|
+      link.tags << Tag.first_or_create(name: tag_name)
+    end
     link.save
     redirect '/links'
   end
