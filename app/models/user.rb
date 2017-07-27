b68f6b2b61d40e0a24715e62402642ff376c7175
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-27 12:50:05 +0100

    Added BCrypt for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
+require 'bcrypt'
+
 # User class (users table) holds data for each user
 class User
   include DataMapper::Resource
 
-  property :id,       Serial
-  property :email,    String
-  property :password, String
+  property :id,               Serial
+  property :email,            String
+  property :password_digest,  Text
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
