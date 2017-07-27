260c86bf1451259c7506a467866f0a325dc7d5e7
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-27 12:10:57 +0100

    Combined scenarios into one in adding a new link

--- spec/features/add_new_link_spec.rb
+++ spec/features/add_new_link_spec.rb
@@ -1,31 +1,17 @@
 require_relative 'web_helper'
 
 feature 'Adding a new link' do
-  before do
+  scenario 'A new link can be added' do
     add_new_link('Makers Academy', 'http://makersacademy.com', 'education')
-  end
-
-  scenario 'Link has a title' do
     expect(current_path).to eq '/links'
     within 'ul#links' do
       expect(page).to have_content('Title: Makers Academy')
-    end
-    expect(Link.last.title).to eq 'Makers Academy'
-  end
-
-  scenario 'Link has a url' do
-    expect(current_path).to eq '/links'
-    within 'ul#links' do
       expect(page).to have_content('URL: http://makersacademy.com')
-    end
-    expect(Link.last.url).to eq 'http://makersacademy.com'
-  end
-
-  scenario 'Link has a tag' do
-    expect(current_path).to eq '/links'
-    within 'ul#links' do
       expect(page).to have_content('Tags: education')
     end
-    expect(Link.last.tags.map(&:name)).to eq ['education']
+    link = Link.last
+    expect(link.title).to eq 'Makers Academy'
+    expect(link.url).to eq 'http://makersacademy.com'
+    expect(link.tags.map(&:name)).to eq ['education']
   end
 end
