74241827f6d458f81696ff5b7ca8e92a7eed7c3f
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-28 09:39:07 +0100

    Corrected error in users/new. Style changes to forms.

--- app/app.rb
+++ app/app.rb
@@ -72,8 +72,8 @@ class BookmarkManager < Sinatra::Base
       session[:user_id] = @user.id
       redirect '/links'
     else
-      flash.now[:errors] = @user.errors.full_messages
-      erb(:'/links/signup')
+      flash.now[:errors] = ['The email or password is incorrect']
+      erb(:'/links/signin')
     end
   end
 
