182271e0a43cebcc6990b126ecb9853d9756a839
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-28 16:04:56 +0100

    Added sign_out_spec file

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,14 @@
+require_relative 'web_helper'
+
+feature 'Signing out' do
+  scenario 'A user can sign out' do
+    user = User.create(email: 'harry@potter.com',
+                       password: 'hogwarts',
+                       password_confirmation: 'hogwarts')
+    sign_in(email: 'harry@potter.com', password: 'hogwarts')
+    expect(current_path).to eq '/links'
+    click_button('Sign Out')
+    expect(page).to have_content 'Goodbye!'
+    expect(page).not_to have_content "Welcome, #{user.email}"
+  end
+end
